0a46c493741077aff2a24888c252f67004a526ef
OctCarp <octcarp@hotmail.com>
2024-03-07 00:12:26 +0800

    Update UserController.java

--- springboot/src/main/java/com/sustech/football/controller/UserController.java
+++ springboot/src/main/java/com/sustech/football/controller/UserController.java
@@ -96,7 +96,7 @@ public class UserController {
     }
 
     @PostMapping("/favorite")
-    @Operation(summary = "收藏", description = "type类型可选：team, player, match, event")
+    @Operation(summary = "收藏", description = "type类型可选：team, user, match, event")
     public void favorite(Long userId, String type, Long id) {
         if (userId == null || type == null || id == null) {
             throw new BadRequestException("参数错误");
@@ -105,25 +105,16 @@ public class UserController {
             throw new BadRequestException("用户不存在");
         }
         switch (type) {
-            case "team":
-                userService.favoriteTeam(userId, id);
-                break;
-            case "player":
-                userService.favoritePlayer(userId, id);
-                break;
-            case "match":
-                userService.favoriteMatch(userId, id);
-                break;
-            case "event":
-                userService.favoriteEvent(userId, id);
-                break;
-            default:
-                throw new BadRequestException("参数错误");
+            case "team"->userService.favoriteTeam(userId, id);
+            case "user"->userService.favoriteUser(userId, id);
+            case "match"->userService.favoriteMatch(userId, id);
+            case "event"->userService.favoriteEvent(userId, id);
+            default->throw new BadRequestException("参数错误");
         }
     }
 
     @PostMapping("/unfavorite")
-    @Operation(summary = "取消收藏", description = "type类型可选：team, player, match, event")
+    @Operation(summary = "取消收藏", description = "type类型可选：team, user, match, event")
     public void unfavorite(Long userId, String type, Long id) {
         if (userId == null || type == null || id == null) {
             throw new BadRequestException("参数错误");
@@ -132,25 +123,16 @@ public class UserController {
             throw new BadRequestException("用户不存在");
         }
         switch (type) {
-            case "team":
-                userService.unfavoriteTeam(userId, id);
-                break;
-            case "player":
-                userService.unfavoritePlayer(userId, id);
-                break;
-            case "match":
-                userService.unfavoriteMatch(userId, id);
-                break;
-            case "event":
-                userService.unfavoriteEvent(userId, id);
-                break;
-            default:
-                throw new BadRequestException("参数错误");
+            case "team" -> userService.unfavoriteTeam(userId, id);
+            case "user" -> userService.unfavoriteUser(userId, id);
+            case "match" -> userService.unfavoriteMatch(userId, id);
+            case "event" -> userService.unfavoriteEvent(userId, id);
+            default -> throw new BadRequestException("参数错误");
         }
     }
 
     @GetMapping("/favorite")
-    @Operation(summary = "获取收藏", description = "type类型可选：team, player, match, event")
+    @Operation(summary = "获取收藏", description = "type类型可选：team, user, match, event")
     public List<?> getFavorite(Long userId, String type) {
         if (userId == null || type == null) {
             throw new BadRequestException("参数错误");
@@ -160,7 +142,7 @@ public class UserController {
         }
         return switch (type) {
             case "team" -> userService.getFavoriteTeams(userId);
-            case "player" -> userService.getFavoritePlayers(userId);
+            case "user" -> userService.getFavoriteUsers(userId);
             case "match" -> userService.getFavoriteMatches(userId);
             case "event" -> userService.getFavoriteEvents(userId);
             default -> throw new BadRequestException("参数错误");
