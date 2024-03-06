09cf9da40a4509678b61509ea2068e1b43ab837b
Haoyu <haoyu.wang141@outlook.com>
2024-03-06 15:02:02 +0800

    .

--- springboot/src/main/java/com/sustech/football/controller/UserController.java
+++ springboot/src/main/java/com/sustech/football/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.sustech.football.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.sustech.football.entity.User;
 import com.sustech.football.exception.BadRequestException;
 import com.sustech.football.exception.ConflictException;
@@ -43,32 +44,34 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Long> login(@RequestHeader(value = "Authorization") String authHeader) {
-        if (authHeader != null && authHeader.startsWith("Basic ")) {
-            String base64Credentials = authHeader.substring("Basic ".length()).trim();
-            byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
-            String credentials = new String(credDecoded, StandardCharsets.UTF_8);
-            String[] values = credentials.split(":", 2);
-
-            String username = values[0];
-            String password = values[1];
-
-            QueryWrapper<User> queryWrapper = new QueryWrapper<>();
-            queryWrapper.like("username", username);
-            User user = userService.getOne(queryWrapper);
-            if (user == null || user.getPassword() == null) {
-                throw new UnauthorizedAccessException("用户名或密码错误");
+    public ResponseEntity<Long> login(String openid, String session_key) {
+        if (openid == null || session_key == null) {
+            throw new BadRequestException("openid和session_key不能为空");
+        }
+//        String access_token = "ACCESS_TOKEN";
+        // signature = hmac_sha256(session_key, "")
+//        String url = "GET https://api.weixin.qq.com/wxa/checksession?access_token=ACCESS_TOKEN&signature=SIGNATURE&openid=OPENID&sig_method=hmac_sha256";
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like("username", openid);
+        User user = userService.getOne(queryWrapper);
+        if (user == null) {
+            user = new User(null, openid, session_key);
+            if (!userService.save(user)) {
+                throw new InternalServerErrorException("注册失败");
             }
-            if (!user.getPassword().equals(password)) {
-                throw new UnauthorizedAccessException("用户名或密码错误");
+        } else {
+            UpdateWrapper<User> updateWrapper = new UpdateWrapper<>();
+            updateWrapper.eq("username", openid).set("password", session_key);
+            if (!userService.update(updateWrapper)) {
+                throw new InternalServerErrorException("???");
             }
-            return ResponseEntity.ok().body(user.getUserId());
         }
 
-        throw new UnauthorizedAccessException("用户名或密码错误");
+        return ResponseEntity.ok().body(user.getUserId());
     }
 
     @PostMapping("/register")
+    @Deprecated
     public ResponseEntity<User> register(String username, String password) {
         if (username == null || password == null) {
             throw new BadRequestException("用户名和密码不能为空");
